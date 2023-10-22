7fe8271146ec4d6df57fd8816ab46541e11939df
widosofthackintosh <tan.nguyen@widosoft.com>
2023-10-22 17:42:19 +0700

    create new api service

--- lib/data/repository/article_repository_impl.dart
+++ lib/data/repository/article_repository_impl.dart
@@ -1,1 +1,5 @@
-class articleRepositoryImpl implements 
+import 'package:clean_architecture/domain/repository/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository {
+
+}
--- lib/domain/repository/article_repository.dart
+++ lib/domain/repository/article_repository.dart
@@ -1,6 +1,9 @@
 import 'package:clean_architecture/core/resources/data_state.dart';
-import 'package:clean_architecture/domain/entities/article.dart';
+import 'package:clean_architecture/data/models/article.dart';
 
 abstract class ArticleRepository {
-  Future<DataState<List<ArticleEntity>>> getNewsArticles();
+  Future<DataState<List<ArticleModel>>> getNewsArticles() {
+    //TODO: implement getNewsArticles
+    throw UnimplementedError();
+  }
 }
